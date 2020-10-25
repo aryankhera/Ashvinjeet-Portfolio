ca8cb95b5ee07cef5fdd1ca03d0c4f3fbeb45891
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2020-10-25 23:07:41 +0530

    first commit

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', (event) => {
+    console.log("Hello World")
+    var leftHeroSection=document.getElementById("left-hero")
+    // leftHeroSection.classList.add("mystyle")
+    var fadedText=document.getElementById("faded-text")
+    fadedText.style.display="block"
+  });
