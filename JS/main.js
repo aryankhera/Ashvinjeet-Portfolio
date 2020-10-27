b719d97fa4f58f5a9c010d5387946c48aef061b5
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2020-10-27 13:43:17 +0530

    Menu animation commit

--- JS/main.js
+++ JS/main.js
@@ -2,11 +2,14 @@ var menuText=document.querySelector(".menu-text")
 var ham1=document.querySelector(".ham-icon1")
 var ham2=document.querySelector(".ham-icon2")
 var hamnavitem = document.querySelector(".ham-nav-item")
+var sidenavitem=document.querySelector(".scroll-time-menu li")
 var tl= gsap.timeline()
 var tl1= gsap.timeline()
+var tl2=gsap.timeline()
+// var rule=CssRulePlugin.getRule(".scroll-time-menu-items:first-child")
 tl.to('#left-hero',1.5,{flex:"0 0 50%"}).to('#faded-text',1.2,{opacity:1})
 tl1.from(menuText,1,{transform:"translate(50px,0px)"}).from(menuText,0.8,{opacity:0},"-=1")
-
+tl2.from('.scroll-time-menu-items',0.5,{transform:"translate(20em,0px)"},"-=0.8")
 
 
 // tl1.from(menuText,1,{transform:"translate(50px,0px)"}).from(menuText,0.8,{opacity:0},"-=1")
@@ -17,10 +20,11 @@ hamnavitem.addEventListener('click',()=>
     ham2.classList.toggle("change")
     hamnavitem.classList.toggle("change")
     tl1.reversed()? tl1.play() :tl1.reverse()
+    tl2.reversed()? tl2.play() :tl2.reverse()
     // ham1.style.transform="rotate(-45deg) translate(0px, 5px)"
     // ham1.style.backgroundColor="black"
     // ham2.style.transform="rotate(45deg) translate(0px, -5px)"
     // ham2.style.backgroundColor="black"
-    
+    // gsap.to('rule',{duration:1,cssRule:{transform:"translate(0px,0px)"}})
 })
 
