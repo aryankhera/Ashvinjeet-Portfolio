da0930dd9f2e29497052c10cfb9d2a2d71c4123c
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2023-03-14 18:06:31 -0400

    menu text changes

--- JS/main.js
+++ JS/main.js
@@ -15,53 +15,60 @@ var tl1 = gsap.timeline();
 var tl2 = gsap.timeline();
 // var rule = CssRulePlugin.getRule(".scroll-time-menu-items:first-child");
 
-tl.to("#mainloader", {
-  height: 0,
+const revealToSpan = () => {
+  document.querySelectorAll(".reveal").forEach((elem) => {
+    console.log("elem", elem);
+    let parent = document.createElement("span");
+    let child = document.createElement("span");
+    parent.classList.add("parent");
+    child.classList.add("child");
+    child.textContent = elem.textContent;
+    parent.appendChild(child);
+    elem.innerHTML = "";
+    elem.appendChild(parent);
+  });
+};
+
+revealToSpan();
+
+tl.to(".child", {
+  y: -100,
   ease: Circ.easeInOut,
-  duration: 1,
-})
-  .to("#brownpage", {
-    top: 0,
-    height: "100%",
-    ease: Circ.easeInOut,
-    duration: 1,
-    delay: -1,
-  })
-  .to("#brownpage", {
-    height: 0,
-    ease: Circ.easeInOut,
-    duration: 2,
-    delay: -0.5,
-  })
-  .to(".follow_cursor", {
-    display: "block",
-  })
-  .to(".translate_div", {
-    transform: "translate(0,-100vh)",
-    height: "0px",
-    ease: Power0.easeIn,
-    delay: -1,
-  })
-  .to(".hero-div", 0.8, { opacity: "1" }, "-=0.1")
-  .to(".left-social-links", 0.5, { opacity: "1" })
-  .to(".right-mail-link", 0.5, { opacity: "1" }, "-=0.5");
+  duration: 5,
+});
+//   .to("#mainloader", {
+//     height: 0,
+//     ease: Circ.easeInOut,
+//     duration: 1,
+//   })
+//   .to("#brownpage", {
+//     top: 0,
+//     height: "100%",
+//     ease: Circ.easeInOut,
+//     duration: 1,
+//     delay: -1,
+//   })
+//   .to("#brownpage", {
+//     height: 0,
+//     ease: Circ.easeInOut,
+//     duration: 2,
+//     delay: -0.5,
+//   })
+//   .to(".follow_cursor", {
+//     display: "block",
+//   })
+//   .to(".translate_div", {
+//     transform: "translate(0,-100vh)",
+//     height: "0px",
+//     ease: Power0.easeIn,
+//     delay: -1,
+//   })
+//   .to(".hero-div", 0.8, { opacity: "1" }, "-=0.1")
+//   .to(".left-social-links", 0.5, { opacity: "1" })
+//   .to(".right-mail-link", 0.5, { opacity: "1" }, "-=0.5");
 
 // Follow cursor
 // gsap.set(cursor, { xPercent: -50, yPercent: -50 });
 // window.addEventListener("mousemove", (e) => {
 //   gsap.to(cursor, { x: e.clientX, y: e.clientY });
 // });
-
-const revealToSpan = () => {
-  document.querySelectorAll(".reveal").forEach = (elem) => {
-    let parent = elem.createElement("span");
-    let child = elem.createElement("span");
-    parent.classList.add("parent");
-    child.classList.add("child");
-
-    child.textContent = elem.textContent;
-    parent.appendChild(child);
-  };
-};
-
-revealToSpan();
