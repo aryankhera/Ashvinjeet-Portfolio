24735b4cca6d9e628656d427459c7f3283e858dc
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2023-10-13 21:20:43 -0400

    scroll added for testing

--- JS/dist/main.dev.js
+++ JS/dist/main.dev.js
@@ -11,7 +11,29 @@ var workBtn = document.querySelector(".workbtn");
 var overlay = document.querySelector(".overlay");
 var Scroll_more = document.getElementById("scroll_mouse");
 var modeChangerSun = document.querySelector("#Sun");
-var modeChangerMoon = document.querySelector("#Moon"); // const toggleSwitch = document.querySelector(
+var modeChangerMoon = document.querySelector("#Moon");
+var scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion:reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach(function (item) {
+    item.setAttribute("data-animated", true);
+    var scrollerInner = item.querySelector(".loop-container");
+    var scrollerContent = Array.from(scrollerInner.children);
+    scrollerInner.style.animationDuration = scrollerInner.offsetWidth / 150 + "s";
+    scrollerContent.forEach(function (i) {
+      var duplicatedItem = i.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+var currentScroll = 0;
+var isScrollingDown = true; // const toggleSwitch = document.querySelector(
 //   '#theme-switch input[type="checkbox"]'
 // );
 
@@ -186,7 +208,101 @@ function detectColorScheme() {
     modeChangerSun.style.display = "none";
     modeChangerMoon.style.display = "inline";
   }
-} // modeChangerSun.addEventListener("click", () => {
+} // class LoopingElement {
+//   constructor(element, currentTranslation, speed) {
+//     this.element = element;
+//     this.currentTranslation = currentTranslation;
+//     this.speed = speed;
+//     this.direction = true;
+//     this.scrollTop = 0;
+//     this.metric = 100;
+//     this.lerp = {
+//       current: this.currentTranslation,
+//       target: this.currentTranslation,
+//       factor: 0.2,
+//     };
+//     this.events();
+//     this.render();
+//   }
+//   events() {
+//     window.addEventListener("scroll", (e) => {
+//       let direction = window.pageYOffset || document.documentElement.scrollTop;
+//       if (direction > this.scrollTop) {
+//         this.direction = true;
+//         this.lerp.target += this.speed * 5;
+//       } else {
+//         this.direction = false;
+//         this.lerp.target -= this.speed * 5;
+//       }
+//       this.scrollTop = direction <= 0 ? 0 : direction;
+//     });
+//   }
+//   lerpFunc(current, target, factor) {
+//     this.lerp.current = current * (1 - factor) + target * factor;
+//   }
+//   goForward() {
+//     this.lerp.target += this.speed;
+//     if (this.lerp.target > this.metric) {
+//       this.lerp.current -= this.metric * 2;
+//       this.lerp.target -= this.metric * 2;
+//     }
+//   }
+//   goBackward() {
+//     this.lerp.target -= this.speed;
+//     if (this.lerp.target < -this.metric) {
+//       this.lerp.current -= -this.metric * 2;
+//       this.lerp.target -= -this.metric * 2;
+//     }
+//   }
+//   animate() {
+//     this.direction ? this.goForward() : this.goBackward();
+//     this.lerpFunc(this.lerp.current, this.lerp.target, this.lerp.factor);
+//     this.element.style.transform = `translateX(${this.lerp.current}%)`;
+//   }
+//   render() {
+//     this.animate();
+//     window.requestAnimationFrame(() => this.render());
+//   }
+// }
+// let elements = document.querySelectorAll(".item");
+// new LoopingElement(elements[0], 100, 0.04);
+// new LoopingElement(elements[1], 0, 0.04);
+// new LoopingElement(elements[2], -100, 0.08);
+// new LoopingElement(elements[3], -100, 0.08);
+// new LoopingElement(elements[4], -100, 0.08);
+// new LoopingElement(elements[5], -100, 0.08);
+// new LoopingElement(elements[6], -100, 0.08);
+// new LoopingElement(elements[7], -100, 0.08);
+// new LoopingElement(elements[8], -100, 0.08);
+// new LoopingElement(elements[9], -100, 0.08);
+// new LoopingElement(elements[10], -100, 0.08);
+// let tween = gsap
+//   .to(".marquee_part", {
+//     x: -100,
+//     repeat: -1,
+//     duration: 5,
+//     ease: "linear",
+//   })
+//   .totalProgress(0.5);
+// gsap.set(".marquee_inner", {
+//   x: -50,
+// });
+// window.addEventListener("scroll", () => {
+//   console.log(window.pageXOffset > currentScroll);
+//   console.log("c", currentScroll);
+//   if (window.pageYOffset > currentScroll) {
+//     isScrollingDown = true;
+//     currentScroll = window.pageYOffset;
+//     console.log("in here");
+//   } else {
+//     isScrollingDown = false;
+//     console.log("out therer");
+//   }
+//   gsap.to(tween, {
+//     timeScale: isScrollingDown ? 1 : -1,
+//   });
+// });
+// modeChangerSun.addEventListener("click", () => {
 //   modeChangerMoon.classList.remove("hidden_class");
 //   modeChangerSun.classList.add("hidden_class");
 // });
